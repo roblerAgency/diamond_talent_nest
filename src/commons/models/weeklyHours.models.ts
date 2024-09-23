665d3beeeba4128039d33246286e2041c2b42ee7
Eduardo Perez Salcedo <a.eduardoperez.fp2019@gmail.com>
2024-09-23 16:29:16 -0400

    🐞[FIX](Users): se agregaban los mismo idiomas y agregamos nuevos valores en el enum de weeklyHours

--- src/commons/models/weeklyHours.models.ts
+++ src/commons/models/weeklyHours.models.ts
@@ -1,7 +1,11 @@
 export enum WEEKLY_HOURS {
-    TWENTY = 20,
-    TWENTY_FIVE = 25,
-    THIRTY = 30,
-    THIRTY_FIVE = 35,
+    FOUR = 4,
+    NINE = 9,
+    FOURTEEN = 14,
+    NINETEEN = 19,
+    TWENTY_FOUR = 24,
+    TWENTY_NINE = 29,
+    THIRTY_FOUR = 34,
+    THIRTY_NINE = 39,
     FORTY = 40
 }
