96cfcd6f908bb2b885245c62a956655ec8e7d7b8
Eduardo Perez Salcedo <a.eduardoperez.fp2019@gmail.com>
2024-10-16 23:05:09 -0400

    ⚡[TEST]: parte VI - Testeamos si resolvimos bug de upload

--- checkUploadFolder.ts
+++ checkUploadFolder.ts
@@ -2,15 +2,16 @@ import { promises as fs } from 'fs';
 import { join } from 'path';
 
 async function checkAndCreateUploadFolder() {
-    const folderPath = join(__dirname, 'upload');
+  // Cambia la ruta a '/app/dist/src/upload'
+  const folderPath = join(__dirname, 'app', 'dist', 'src', 'upload');
 
-    try {
-        await fs.access(folderPath);
-        console.info('La carpeta "upload" ya existe.');
-    } catch (error) {
-        await fs.mkdir(folderPath);
-        console.info('La carpeta "upload" ha sido creada.');
-    }
+  try {
+    await fs.access(folderPath);
+    console.info('La carpeta "upload" ya existe.');
+  } catch (error) {
+    await fs.mkdir(folderPath, { recursive: true }); // Crea la carpeta y cualquier carpeta padre necesaria
+    console.info('La carpeta "upload" ha sido creada.');
+  }
 }
 
 checkAndCreateUploadFolder().catch(console.error);
