a3dd35f06e63436f906067e3ecf7fcadd7185523
roblerAgency <info@robleragency.com>
2025-07-21 12:11:34 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,8 @@
-// robleragency/diamond_talent_nest/diamond_talent_nest-77af46dc787f9e21f2b91b8de8a192492e3fc974/src/database/database.module.ts
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigType } from '@nestjs/config';
+
+// Config
 import config from 'config/config';
 
 @Global()
@@ -19,18 +20,8 @@ import config from 'config/config';
           password,
           database: name,
           autoLoadEntities: true,
+          synchronize: true,
           synchronize: false,
-          keepConnectionAlive: true,
-          extra: {
-            connectionLimit: 10,      
-            connectTimeout: 20000,  
-            acquireTimeout: 20000,   
-          },
-          // -----------------------------
         };
       },
     }),
-  ],
-  exports: [TypeOrmModule],
-})
-export class DatabaseModule {}
