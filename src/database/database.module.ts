bb554186767a73359aafc1437427e1cb85aad658
roblerAgency <info@robleragency.com>
2025-07-21 12:32:06 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,7 @@
+// robleragency/diamond_talent_nest/diamond_talent_nest-77af46dc787f9e21f2b91b8de8a192492e3fc974/src/database/database.module.ts
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigType } from '@nestjs/config';
-
 import config from 'config/config';
 
 @Global()
@@ -19,8 +19,11 @@ import config from 'config/config';
           password,
           database: name,
           autoLoadEntities: true,
-          synchronize: true,
-          synchronize: false,
+          synchronize: false, // <-- MUY IMPORTANTE que esté en 'false'
+          
+          // --- Estrategia de reintentos para mantener la conexión ---
+          retryAttempts: 10,
+          retryDelay: 3000,
         };
       },
     }),
