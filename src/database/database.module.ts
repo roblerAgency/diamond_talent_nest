e2e75b28a2676a14d0a10833114159bad2755555
roblerAgency <info@robleragency.com>
2025-07-21 12:02:28 -0400

    Update database.module.ts es mas robusta

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,7 @@
+// robleragency/diamond_talent_nest/diamond_talent_nest-77af46dc787f9e21f2b91b8de8a192492e3fc974/src/database/database.module.ts
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigType } from '@nestjs/config';
-
-// Config
 import config from 'config/config';
 
 @Global()
@@ -21,6 +20,14 @@ import config from 'config/config';
           database: name,
           autoLoadEntities: true,
           synchronize: false,
+          // --- AÑADE ESTAS LÍNEAS ---
+          keepConnectionAlive: true, // <-- Mantiene la conexión viva
+          extra: {
+            connectionLimit: 10, // <-- Usa un pool de conexiones
+            idleTimeoutMillis: 60000, // <-- Cierra conexiones inactivas después de 60s
+            connectTimeout: 10000 // <-- Tiempo de espera para la conexión inicial
+          },
+          // --------------------------
         };
       },
     }),
