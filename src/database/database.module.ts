93b7755fa1e563940a56eee6c54f4fce17609589
roblerAgency <info@robleragency.com>
2025-07-22 04:20:01 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,16 @@
+// src/database/database.module.ts
 import { Global, Module } from '@nestjs/common';
-import { ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import config from 'config/config';
+import config from 'config';
 
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [config.KEY],
-      useFactory: (configService: ConfigType<typeof config>) => ({
+      useFactory: (configService) => ({
         type: 'mysql',
-        url: configService.database.url,
+        url: configService.get('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: false,
       }),
