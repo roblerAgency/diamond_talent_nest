3731fa8baa6931dd6e67e0f606fe7e4b84e42a36
roblerAgency <info@robleragency.com>
2025-07-22 04:12:10 -0400

    Update validationSchema.ts

--- config/validationSchema.ts
+++ config/validationSchema.ts
@@ -1,7 +1,8 @@
+// config/env.validation.ts
 import * as Joi from 'joi';
 
 export const configSchema = Joi.object({
-  NODE_ENV: Joi.string().required(),
-  DATABASE_URL: Joi.string().uri().required(), // ← único campo DB
-  JWT_SECRET: Joi.string().required(),
+  NODE_ENV:     Joi.string().required(),
+  DATABASE_URL: Joi.string().required(), // 👈 just string, not uri()
+  JWT_SECRET:   Joi.string().required(),
 });
