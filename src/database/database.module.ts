261f14ae685fdadf3750010bb9128d20e1ed87a0
roblerAgency <info@robleragency.com>
2025-07-22 04:31:03 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,7 +10,7 @@ import { ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql',
-        url: configService.get('DATABASE_URL'), // Asegúrate de que esta variable esté configurada
+        url: configService.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: false,
       }),
