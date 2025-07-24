d9a41688fb5bf528cb6b6228205f575b3920404d
roblerAgency <info@robleragency.com>
2025-07-24 09:05:13 -0400

    Update config.ts Downgrade JWT

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,12 @@
-// src/config/config.ts
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => ({
   database: {
-    url: process.env.DATABASE_URL,
+    name: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT, 10),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
   },
   jwtSecret: process.env.JWT_SECRET,
 }));
