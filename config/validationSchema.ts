3f9f3acd134766aa08b0badc8a0149010e7edf00
roblerAgency <info@robleragency.com>
2025-07-24 09:03:41 -0400

    Update validationSchema.ts Downgrade

--- config/validationSchema.ts
+++ config/validationSchema.ts
@@ -1,8 +1,11 @@
-// src/config/validationSchema.ts
 import * as Joi from 'joi';
 
 export const configSchema = Joi.object({
   NODE_ENV: Joi.string().required(),
-  DATABASE_URL: Joi.string().required(), // Solo requiere que sea una cadena
+  DB_DATABASE: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_HOST: Joi.string().hostname().required(),
   JWT_SECRET: Joi.string().required(),
 });
